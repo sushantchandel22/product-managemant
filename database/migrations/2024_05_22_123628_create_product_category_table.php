03f76a20caaf34191885a388b935ba794b0b88bf
sushantchandel22 <sushantchandel119@gmail.com>
2024-05-22 19:03:51 +0530

    performing migration

--- database/migrations/2024_05_22_123628_create_product_category_table.php
+++ database/migrations/2024_05_22_123628_create_product_category_table.php
@@ -11,12 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('product_category', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->string('price');
-            $table->string('image');
-            $table->string('description');
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -26,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('product_category');
     }
 };
